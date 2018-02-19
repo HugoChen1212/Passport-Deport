cfa7554715722b7e18b729e0103ebd078c019120
Hugo <hugochen1212@gmail.com>
2018-02-18 22:39:58 -0800

    update addree addres font size

--- js/application.js
+++ js/application.js
@@ -11,23 +11,48 @@ $(document).ready(function (){
 
 
 var images = [
-  "img/flight.jpg",
-  "img/balloon.jpg",
-  "img/Travel-site.jpg"
+  '../img/flight.jpg',
+  '../img/balloon.jpg',
+  '../img/Travel-site.jpg'
 ]
 
 var imageHead = document.getElementById("image-head");
-var i = 0;
 
-setInterval(function() {
-      imageHead.style.backgroundImage = "url(" + images[i] + ")";
-      i = i + 1;
-      if (i == images.length) {
+
+var i = 0;
+function setImage(){
+
+imageHead.style.backgroundImage = "url(" + images[i] + ")";
+
+i = i + 1;
+
+ if (i === images.length) {
         i =  0;
       }
-}, 5000);
+
+}
+
+setInterval(setImage, 5000);
 
 
 
+
+
+// $(document).ready(function(){
+//   $('#image-head').fadeToggle(3000);
+
+// });
+
+
+// $(document).ready(function () {
+//   var imagesfile=['../img/flight.jpg','../img/balloon.jpg','../img/Travel-site.jpg'];
+//   var currentIddex =0;
+//   setInterval (function(){
+//     if(currentIndex==imageFile.length){
+//       currentIndex =0;
+//     }
+//     $("#image-head").css('background-image', "url('" + imageFile[currentIndex++] + "')");
+//   }, 2000);
+// });
 
 
