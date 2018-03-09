357766f655de09abf97dd2469222d79f955ab2a6
Hugo <hugochen1212@gmail.com>
2018-03-09 03:57:29 -0800

    add business work and student visa details

---
+++ js/chinese_visa.js
@@ -0,0 +1,138 @@
+//jquery for new_passport
+ $('.panel').hide();
+ $('.panels').children().first().show();
+  $(".np-list-item").click( function(){
+    var self = $(this);
+    var currentIndex = self.index();
+    $(".np-list-item").css({"background-color":"#e0e1e2","border-bottom":"1px solid #e0e0e0","height": "50px"});
+     $('.panels').children().hide();
+    $('.panels').children().eq(currentIndex).show();
+    $(self).css({"background-color":"#f9f9f9", "border-bottom": "1px solid #f9f9f9","height": "60px"});
+  });
+
+//jquery for chinese Tourist visa
+//jquery for chinese Tourist visa
+
+$('.hide-tourist-plant').hide();
+$(".visa-type").click( function(){
+  var self=$(this);
+  var currentIndex = self.index();
+  $(".visa-type").css({"border-right":"1px solid #e0e0e0"});
+  $('.tourist-visa-plants').children().hide();
+  $('.tourist-visa-plants').children().eq(currentIndex).show();
+  $(self).css({"border-right": "1px solid #f9f9f9"});
+});
+
+
+//Jauery for chinese Tourist visa detial
+$('.hide-visa-detail').hide();
+$(".nav-visa-detail").click( function(){
+  var self=$(this);
+  var currentIndex = self.index();
+  $(".nav-visa-detail").css({"border-bottom":"1px solid #f9f9f9"});
+  $('.visa-detail-plants').children().hide();
+  $('.visa-detail-plants').children().eq(currentIndex).show();
+  $(self).css({"border-bottom": "3px solid #5f4696"});
+});
+
+
+//Jauery for chinese Business visa detial
+
+$('.hide-visa-detail').hide();
+$(".nav-visa-detail").click( function(){
+  var self=$(this);
+  var currentIndex = self.index();
+  $(".nav-visa-detail").css({"border-bottom":"1px solid #f9f9f9"});
+
+  $(".bv-detail-plants").children().hide();
+  $(".bv-detail-plants").children().eq(currentIndex).show();
+  $(self).css({"border-bottom": "3px solid #5f4696"});
+});
+
+//Jauery for chinese work visa detial
+
+$('.hide-visa-detail').hide();
+$(".nav-visa-detail").click( function(){
+  var self=$(this);
+  var currentIndex = self.index();
+  $(".nav-visa-detail").css({"border-bottom":"1px solid #f9f9f9"});
+
+  $(".wv-detail-plants").children().hide();
+  $(".wv-detail-plants").children().eq(currentIndex).show();
+  $(self).css({"border-bottom": "3px solid #5f4696"});
+});
+
+
+//Jauery for chinese student visa detial
+
+$('.hide-visa-detail').hide();
+$(".nav-visa-detail").click( function(){
+  var self=$(this);
+  var currentIndex = self.index();
+  $(".nav-visa-detail").css({"border-bottom":"1px solid #f9f9f9"});
+
+  $(".sv-detail-plants").children().hide();
+  $(".sv-detail-plants").children().eq(currentIndex).show();
+  $(self).css({"border-bottom": "3px solid #5f4696"});
+});
+
+//jQuery for Chinese Tourist visa details titel dropdown
+
+$(".plus-side").closest(".dp-title").click( function(){
+  $(".dp-title").hide();
+  $(".dm-details").show();
+});
+
+$(".minus-side").closest(".dm-details").click( function(){
+  $(".dm-details").hide();
+  $(".dp-title").show();
+});
+
+//jQuery for Chinese Tourist visa born dropdown
+
+$(".born-plus-side").closest(".born-title").click( function(){
+  $(".born-title").hide();
+  $(".born-details").show();
+});
+
+$(".born-minus-side").closest(".born-details").click( function(){
+  $(".born-details").hide();
+  $(".born-title").show();
+});
+
+
+//jQuery for Chinese Tourist visa adopted dropdown
+
+$(".adopted-plus-side").closest(".adopted-title").click( function(){
+  $(".adopted-title").hide();
+  $(".adopted-details").show();
+});
+
+$(".adopted-minus-side").closest(".adopted-details").click( function(){
+  $(".adopted-details").hide();
+  $(".adopted-title").show();
+});
+
+//jQuery for Chinese Tourist visa tibetan dropdown
+
+$(".tibetan-plus-side").closest(".tibetan-title").click( function(){
+  $(".tibetan-title").hide();
+  $(".tibetan-details").show();
+});
+
+$(".tibetan-minus-side").closest(".tibetan-details").click( function(){
+  $(".tibetan-details").hide();
+  $(".tibetan-title").show();
+});
+
+//jQuery for Chinese Tourist visa media dropdown
+
+$(".media-plus-side").closest(".media-title").click( function(){
+  $(".media-title").hide();
+  $(".media-details").show();
+});
+
+$(".media-minus-side").closest(".media-details").click( function(){
+  $(".media-details").hide();
+  $(".media-title").show();
+});
